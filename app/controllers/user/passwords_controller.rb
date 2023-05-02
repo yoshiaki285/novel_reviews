056e06c45b5889cdedf109a154f15f17ebc0a13a
EC2 Default User <ec2-user@ip-172-31-36-154.ap-northeast-1.compute.internal>
2023-05-02 01:00:22 +0000

    Fix a typo

--- app/controllers/user/passwords_controller.rb
+++ app/controllers/user/passwords_controller.rb
@@ -1,5 +1,5 @@
 class User::PasswordsController < Devise::PasswordsController
-  before_action :ensure_normal_user, only: create
+  before_action :ensure_normal_user, only: :create
   
   def ensure_normal_user
     if params[:user][:email].downcase == 'guest@example.com'
