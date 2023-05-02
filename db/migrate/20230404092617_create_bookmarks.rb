75e62704c0bfa7670dbb51b1a66a8e3a6a084c31
EC2 Default User <ec2-user@ip-172-31-36-154.ap-northeast-1.compute.internal>
2023-05-02 02:31:05 +0000

    Update migrate bookmarks

--- db/migrate/20230404092617_create_bookmarks.rb
+++ db/migrate/20230404092617_create_bookmarks.rb
@@ -2,7 +2,7 @@ class CreateBookmarks < ActiveRecord::Migration[6.1]
   def change
     create_table :bookmarks do |t|
       t.integer :user_id
-      t.integer :book_id
+      t.bigint :book_id
 
       t.timestamps
     end
